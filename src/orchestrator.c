4a5c169397f2d02173d3e95b1b99c663cd911235
Nuno Ribeiro <a104177@alunos.uminho.pt>
2024-04-12 16:40:20 +0100

    Small corrections

--- src/orchestrator.c
+++ src/orchestrator.c
@@ -5,19 +5,20 @@
 
 int main(int argc, char* argv[]){
     char command[COMMANDSIZE];
+
+    if(mkfifo("fifo",0666) == -1) perror("Erro ao criar pipeline"); // ligar o server
+
+
     // execute time -u "prog-a [args]"
     if(argc < 2) {
         perror("Falta de argumentos");
         return -1;
     }
 
-    if(my_strcmp(argv[1],"-") == 0){
-        if (my_strcmp(argv[2],"help") == 0){
-            my_strcpy(command, "ajuda");
-            printf("teste\n"); // neste momento usado para debug
-        } else {
-            printf("Invalid argument\n");
-        }
+    if(my_strcmp(argv[1],"-help") == 0){
+        my_strcpy(command, "-help");
+        printf("teste\n"); // neste momento usado para debug
+        printf("Comando: %s\n",command);
     } else {
 
     }
--- src/server.c
+++
@@ -1,6 +0,0 @@
-#include <client.h>
-
-int main(){
-    if(mkfifo("fifo",0666) == -1) perror("Erro ao criar pipeline");
-    return 0;
-}
