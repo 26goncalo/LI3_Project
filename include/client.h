946df27e04aabca0d2bb30d3cd56ac10d7bf1800
GonçaloCruz <a104346@alunos.uminho.pt>
2024-04-14 23:40:04 +0100

    Criação dos ficheiros de output

--- include/client.h
+++ include/client.h
@@ -5,3 +5,4 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
--- src/orchestrator.c
+++ src/orchestrator.c
@@ -39,6 +39,10 @@ int main(int argc, char* argv[]){
         }
         mkdir(output_folder, 0777);
         int client_to_server = open("client_to_server", O_RDONLY);
+        int nr_task = 1;
+        char tasks_file[60];
+        sprintf(tasks_file, "./%s/ALL_TASKS.txt", output_folder);
+        int tasks = open(tasks_file, O_RDWR | O_CREAT | O_APPEND, 0666);
         while(1){   
             int bytes_read = 0;
             char buf[1000];
@@ -77,17 +81,67 @@ int main(int argc, char* argv[]){
                     if(i > 3){  //  ./client execute time -u "prog-a [args]"
                         if((strcmp(args[0], "execute") == 0)  &&  ((strcmp(args[2], "-u") == 0) || (strcmp(args[2], "-p") == 0))){
                             int time = atoi(args[1]);
-                            if(strcmp(args[2], "-u") == 0){  // "-u"
-                                for(int j = 0; j<i; j++){
-                                    write(server_to_client, args[j], strlen(args[j]));
-                                    if(j!=i-1) write(server_to_client, " ", 1);
+                            
+                            char* args_prog[20][20];   //  args_prog[número do programa][número do argumento]
+                            int NR_P = 0, nr_p = 0, nr_arg = 0;
+                            for(int j = 3; j<i; j++){
+                                if(strcmp(args[j], "|") == 0){
+                                    args_prog[nr_p][nr_arg] = NULL;
+                                    nr_p++;
+                                    NR_P++;
+                                    nr_arg = 0;
                                 }
-                                write(server_to_client, "\n", 1);
+                                else{
+                                    args_prog[nr_p][nr_arg] = strdup (args[j]);
+                                    nr_arg++;
+                                }
+                            }
+                            args_prog[nr_p][nr_arg] = NULL;
+                            nr_p++;
+                            NR_P++;
+                            nr_arg = 0;
+                            
+                            char message[20];
+                            sprintf(message, "\nTASK %d Received\n\n", nr_task);
+                            write(server_to_client, message, strlen(message));
+                            
+                            char taskX_file[60];
+                            sprintf(taskX_file, "./%s/TASK%d.txt", output_folder, nr_task);
+                            int taskX = open(taskX_file, O_RDWR | O_CREAT, 0666);
+
+                            if(strcmp(args[2], "-u") == 0){  // "-u"
+                                int status;
+                                int pid = fork();
+                                if(pid == -1){
+                                    write(server_to_client, "Erro na criação do processo-filho\n", 37);
+                                    close(server_to_client);
+                                }
+
+                                if(pid == 0){ //processo-filho
+                                    dup2(taskX, 1);
+                                    dup2(taskX, STDERR_FILENO);
+                                    execvp(args_prog[0][0], args_prog[0]);
+                                    write(1, "Erro na execução do programa\n", 31);
+                                    _exit(255);
+                                }
+                                wait(&status);
+                                char task_completed[50];
+                                sprintf(task_completed, "TASK %d was completed in %d milliseconds\n", nr_task, 0);    //DIZER QUANTOS MILISSEGUNDOS DEMOROU
+                                write(tasks, task_completed, strlen(task_completed));
                                 close(server_to_client);
                             }
+
                             else{   // "-p"
                                 // ...
                             }
+
+                            for(int nr_p = 0; nr_p<NR_P ; nr_p++){
+                                for(int nr_arg = 0; nr_arg<20 && args_prog[nr_p][nr_arg]!=NULL; nr_arg++){
+                                    free(args_prog[nr_p][nr_arg]);
+                                }
+                            }
+                            nr_task++;
+                            close(taskX);
                         }
                         else{
                             write(server_to_client, "Comando inválido\n", 18);
@@ -101,6 +155,7 @@ int main(int argc, char* argv[]){
                 }
             }
         }
+        close(tasks);
     }
     printf("Argumentos inválidos\n");
     return -1;
