1ec9998f947afb332d4700c978c1eefab562b163
GonçaloCruz <a104346@alunos.uminho.pt>
2024-04-27 23:31:31 +0100

    Final commit

--- pscript.sh
+++ pscript.sh
@@ -19,33 +19,45 @@ function opcao_aleatoria() {
 function rodar_comando() {
     valor=$(valor_aleatorio)
     opcao=$(opcao_aleatoria)
-    programas=("sleep" "oi" "ls" "ls -l" "pwd" "date" "whoami" "hostname" "df" "uname -a" "du -h" "uptime" "man ls" "who" "w" "last")
-    
-    if [ "$opcao" == "-p" ]; then
-        # Se a opção for -p, criar uma pipeline de programas
-        pipeline=""
-        num_programas=$((RANDOM % 4 + 2))  # Entre 2 e 5 programas aleatoriamente
-        for ((i = 0; i < num_programas; i++)); do
+    if [ $i -eq 1 ]; then
+        comando="./client execute $valor -u sleep 2"
+        echo "Comando: $comando"
+        eval $comando
+    else
+        programas=("sleep" "exemplo" "ls" "ls -l" "pwd" "date" "whoami" "hostname" "df" "uname -a" "du -h" "uptime" "man ls" "who" "w")
+
+        if [ "$opcao" == "-p" ]; then
+            # Se a opção for -p, criar uma pipeline de programas
+            pipeline=""
+            num_programas=$((RANDOM % 4 + 2))  # Entre 2 e 5 programas aleatoriamente
+            for ((i = 0; i < num_programas; i++)); do
+                programa=${programas[$RANDOM % ${#programas[@]}]}
+                if [ "$programa" == "sleep" ]; then
+                    argumento=$((RANDOM % 20 + 1))
+                    pipeline+="$programa $argumento"
+                else
+                    pipeline+="$programa"
+                fi
+                if [ $i -lt $(($num_programas - 1)) ]; then
+                    pipeline+=" | "
+                fi
+            done
+            comando="./client execute $valor $opcao \"$pipeline\""
+        else
+            # Se a opção for -u, selecionar apenas um programa
             programa=${programas[$RANDOM % ${#programas[@]}]}
             if [ "$programa" == "sleep" ]; then
-                argumento=$(valor_aleatorio)
-                pipeline+="$programa $argumento"
+                argumento=$((RANDOM % 20 + 1))
+                comando="./client execute $valor $opcao \"$programa $argumento\""
             else
-                pipeline+="$programa"
+                comando="./client execute $valor $opcao \"$programa\""
             fi
-            if [ $i -lt $(($num_programas - 1)) ]; then
-                pipeline+=" | "
-            fi
-        done
-        comando="./client execute $valor $opcao \"$pipeline\""
-    else
-        # Se a opção for -u, selecionar apenas um programa
-        programa=${programas[$RANDOM % ${#programas[@]}]}
-        comando="./client execute $valor $opcao \"$programa\""
-    fi
+        fi
 
     echo "Comando: $comando"
     eval $comando
+    
+    fi
 }
 
 # Roda o comando 5 vezes com valores aleatórios
